f7c2079e81c6ad9aab0a5888c598ac048b5e58ab
pnxdxt <paul.nodet@gmail.com>
2015-12-07 15:14:26 +0100

    day 7

---
+++ src/2015/day-07/part-02.ts
@@ -0,0 +1,38 @@
+const TEST = false;
+const file = await Deno.readTextFile(TEST ? "./sample.txt" : "./input.txt");
+const input = file.split("\n");
+
+const converter = {
+  AND: (a: number, b: number) => a & b,
+  OR: (a: number, b: number) => a | b,
+  LSHIFT: (a: number, b: number) => a << b,
+  RSHIFT: (a: number, b: number) => a >> b,
+  NOT: (_a: number, b: number) => b ^ 65535,
+  VAL: (_a: number, b: number) => b,
+};
+
+const wires: Record<string, number> = {
+  b: 46065,
+};
+
+// deno-lint-ignore no-prototype-builtins
+const test = (i: string) => !i || wires.hasOwnProperty(i) || /\d+/.test(i);
+const val = (i: string) => wires[i] || +i;
+
+while (input.length) {
+  const [o, a, operator, b, c] = input?.shift()?.match(
+    /([a-z0-9]*)\b\s?([A-Z]+)?\s?(\S+)\s->\s(\S+)/,
+  ) ?? [];
+
+  if (!test(c)) {
+    if (test(a) && test(b)) {
+      wires[c] = converter[operator as keyof typeof converter || "VAL"](
+        val(a),
+        val(b),
+      );
+    } else input.push(o);
+  }
+}
+
+const result = wires.a;
+console.log("result", result);
