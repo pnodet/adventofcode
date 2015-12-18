a1b73d3908be66f901d8e8302cba1eaebe033c10
pnxdxt <paul.nodet@gmail.com>
2015-12-18 13:16:45 +0100

    day 18

---
+++ src/2015/day-18/part-01.ts
@@ -0,0 +1,29 @@
+const file = await Deno.readTextFile("./input.txt");
+const input = file.split("\n");
+
+const c = [-1, 0, 1];
+const t: Record<number, number[]> = { 1: [2, 3], 0: [3] };
+
+const matrix = input.map((line) =>
+  line.split("").map((cell) => cell === "#" ? 1 : 0)
+);
+
+const neighbors = (matrix: number[][], x: number, y: number) =>
+  c.reduce(
+    (r, a) =>
+      r + c.reduce((r, b) => r + ((matrix[x + a] || {})[y + b] || 0), 0),
+    0,
+  ) - matrix[x][y];
+
+const iterate = (r: number[]) =>
+  r.map((_, x: number) =>
+    // @ts-expect-error Speed over type safety
+    r.map((_, y: number) => +!!~t[r[x][y]].indexOf(neighbors(r, x, y)))
+  );
+
+const result = [...Array(100)].reduce(iterate, matrix).reduce(
+  (r: number, v: number[]) => r + v.reduce((r: number, v: number) => r + v, 0),
+  0,
+);
+
+console.log("result", result);
--- src/utils/new-day.ts
+++ src/utils/new-day.ts
@@ -1,4 +1,4 @@
-const DAY = 17;
+const DAY = 18;
 const YEAR = 2015;
 
 const template = (): Uint8Array => {
